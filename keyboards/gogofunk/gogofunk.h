2657430db6ebbcf76cf9614385fae01e965b7f0b
marksard <marksard@rocketmail.com>
2018-10-09 13:31:10 +0900

    add new keyboard gogofunk

---
+++ keyboards/gogofunk/gogofunk.h
@@ -0,0 +1,60 @@
+/* Copyright 2018 REPLACE_WITH_YOUR_NAME
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef GOGOFUNK_H
+#define GOGOFUNK_H
+
+#include "quantum.h"
+
+/* This a shortcut to help you visually see your layout.
+ *
+ * The first section contains all of the arguments representing the physical
+ * layout of the board and position of the keys.
+ *
+ * The second converts the arguments into a two-dimensional array which
+ * represents the switch matrix.
+ */
+#define LAYOUT( \
+  L00, L01, L02, L03, L04, \
+  L10, L11, L12, L13, L14, \
+  L20, L21, L22, L23, L24, \
+  L30, L31, L32, L33, L34, \
+  L40, L41, L42, L43, L44  \
+  ) \
+  { \
+    { L04, L03, L02, L01, L00 }, \
+    { L14, L13, L12, L11, L10 }, \
+    { L24, L23, L22, L21, L20 }, \
+    { L34, L33, L32, L31, L30 }, \
+    { L44, L43, L42, L41, L40 } \
+  }
+
+// Used to create a keymap using only KC_ prefixed keys
+#define LAYOUT_kc( \
+  L00, L01, L02, L03, L04, \
+  L10, L11, L12, L13, L14, \
+  L20, L21, L22, L23, L24, \
+  L30, L31, L32, L33, L34, \
+  L40, L41, L42, L43, L44 \
+  ) \
+  LAYOUT( \
+    KC_##L00, KC_##L01, KC_##L02, KC_##L03, KC_##L04, \
+    KC_##L10, KC_##L11, KC_##L12, KC_##L13, KC_##L14, \
+    KC_##L20, KC_##L21, KC_##L22, KC_##L23, KC_##L24, \
+    KC_##L30, KC_##L31, KC_##L32, KC_##L33, KC_##L34, \
+    KC_##L40, KC_##L41, KC_##L42, KC_##L43, KC_##L44 \
+  )
+
+#endif
---
+++ keyboards/gogofunk/keymaps/default/keymap.c
@@ -0,0 +1,88 @@
+/* Copyright 2018 REPLACE_WITH_YOUR_NAME
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+enum layer_number {
+  _BASE = 0,
+};
+
+// Defines the keycodes used by our macros in process_record_user
+enum custom_keycodes {
+  QMKBEST = SAFE_RANGE,
+  QMKURL
+};
+
+#define KC______ KC_TRNS
+#define KC_XXXXX KC_NO
+#define KC_KANJI KC_GRV
+
+#define KC_RST   RESET
+
+#define KC_RSBR  LSFT(KC_8)
+#define KC_REBR  LSFT(KC_9)
+
+#define KP_00 0	// keypad "double 0"
+#define KC_KP00  M(KP_00)
+
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_BASE] = LAYOUT_kc(
+  //,-----------------------------------
+       NLCK,  PSLS,  PAST,  BSPC,    F1,\
+  //|------+------+------+------+------|
+         P7,    P8,    P9,  PMNS,    F2,\
+  //|------+------+------+------+------|
+         P4,    P5,    P6,  PPLS,    F3,\
+  //|------+------+------+------+------|
+         P1,    P2,    P3,  PPLS,    F4,\
+  //|------+------+------+------+------|
+         P0,  KP00,  PDOT,  PENT,    F5 \
+  //|------+------+------+------+-------
+  ),
+};
+
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
+
+  switch (id) {
+    // keypad "double 0"
+    case KP_00:
+      if (record->event.pressed) {
+        return MACRO( T(KP_0), D(KP_0), END );
+      } else {
+        return MACRO( U(KP_0), END );
+      }
+      break;
+  }
+
+  return MACRO_NONE;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+
+  return true;
+}
+
+void matrix_init_user(void) {
+
+}
+
+void matrix_scan_user(void) {
+
+}
+
+void led_set_user(uint8_t usb_led) {
+
+}
