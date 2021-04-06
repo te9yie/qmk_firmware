7e35c6c288fd0a5c8e3430425fba29157626da18
te9yie <te9yie@users.noreply.github.com>
2021-04-06 11:18:11 +0900

    Update

--- keyboards/miniaxe/keymaps/te9yie/keymap.c
+++ keyboards/miniaxe/keymaps/te9yie/keymap.c
@@ -26,14 +26,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------|           |------+------+------+------+------|
  * |   Z  |Alt/X |   C  |   V  |   B  |           |   N  |   M  |   ,  |Alt/. |   /  |
  * `-------------+------+------+------|           |------+------+------+------+------'
- *               |LT3/GUI|LT2/Del|Sft/Ent|        |LT1/Spc| F13 | GUI  |
+ *               |LT3/GUI|LT2/F13|Sft/Ent|        |LT1/Spc| F14 | GUI  |
  *               `--------------------'           `--------------------'
  */
 [0] = LAYOUT( \
     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,
     LCTL_T(KC_A),KC_S,KC_D, KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   RCTL_T(KC_SCLN),
     KC_Z,   LALT_T(KC_X),KC_C,KC_V,KC_B,KC_N,  KC_M,   KC_COMM,RALT_T(KC_DOT),KC_SLSH,
-                    LT(3,KC_LGUI),LT(2,KC_DEL),SFT_T(KC_ENT),LT(1,KC_SPC),KC_F13,KC_RGUI
+                    LT(3,KC_LGUI),LT(2,KC_F13),SFT_T(KC_ENT),LT(1,KC_SPC),KC_F14,KC_RGUI
 ),
 
 /*
