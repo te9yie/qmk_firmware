6b6285a4ded2d9da1e94ae8d7011ab2e2d84f646
te9yie <te9yie@users.noreply.github.com>
2021-05-03 19:36:20 +0900

    update

--- keyboards/miniaxe/keymaps/te9yie/keymap.c
+++ keyboards/miniaxe/keymaps/te9yie/keymap.c
@@ -22,17 +22,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,----------------------------------.           ,----------------------------------.
  * |   Q  |   W  |   E  |   R  |   T  |           |   Y  |   U  |   I  |   O  |   P  |
  * |------+------+------+------+------|           |------+------+------+------+------|
- * |Ctl/A |Alt/S |   D  |   F  |   G  |           |   H  |   J  |   K  |Alt/L |Ctl/; |
+ * |Ctl/A |   S  |   D  |   F  |   G  |           |   H  |   J  |   K  |   L  |Ctl/; |
  * |------+------+------+------+------|           |------+------+------+------+------|
- * |   Z  |   X  |   C  |   V  |   B  |           |   N  |   M  |   ,  |   .  |   /  |
+ * |   Z  |Alt/X |   C  |   V  |   B  |           |   N  |   M  |   ,  |Alt/. |   /  |
  * `-------------+------+------+------|           |------+------+------+------+------'
  *               |LT3/GUI|LT2/F13|Sft/Ent|        |LT1/Spc| F14 | GUI  |
  *               `--------------------'           `--------------------'
  */
 [0] = LAYOUT( \
     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,
-    LCTL_T(KC_A),LALT_T(KC_S),KC_D,KC_F,KC_G,KC_H,  KC_J,   KC_K,   RALT_T(KC_L),RCTL_T(KC_SCLN),
-    KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,
+    LCTL_T(KC_A),KC_S,KC_D, KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   RCTL_T(KC_SCLN),
+    KC_Z,   LALT_T(KC_X),KC_C,KC_V, KC_B,   KC_N,   KC_M,   KC_COMM,RALT_T(KC_DOT), KC_SLSH,
                     LT(3,KC_LGUI),LT(2,KC_F13),SFT_T(KC_ENT),LT(1,KC_SPC),KC_F14,KC_RGUI
 ),
 
@@ -40,17 +40,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,----------------------------------.           ,----------------------------------.
  * |   1  |   2  |   3  |   4  |   5  |           |   6  |   7  |   8  |   9  |   0  |
  * |------+------+------+------+------|           |------+------+------+------+------|
- * |Ctl/` |      | Del  |      |      |           | BkSp |Enter | Tab  |Alt/Esc|Ctl/'|
+ * |Ctl/` |      | Del  |      |      |           | BkSp |Enter | Tab  | Esc  |Ctl/' |
  * |------+------+------+------+------|           |------+------+------+------+------|
- * |      |      |      |      |      |           |   -  |   =  |   [  |   ]  |   \  |
+ * |      |      |      |      |      |           |   -  |   =  |   [  |Alt/] |   \  |
  * `-------------+------+------+------|           |------+------+------+------+------'
  *               |      |      |      |           |      |      |      |
  *               `--------------------'           `--------------------'
  */
 [1] = LAYOUT( \
     KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,
-    LCTL_T(KC_GRV),KC_TRNS,KC_DEL, KC_TRNS,KC_TRNS,KC_BSPC,KC_ENT,KC_TAB,RALT_T(KC_ESC),RCTL_T(KC_QUOT),
-    KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_MINS,KC_EQL, KC_LBRC,KC_RBRC,KC_BSLS,
+    LCTL_T(KC_GRV),KC_TRNS,KC_DEL, KC_TRNS,KC_TRNS,KC_BSPC,KC_ENT,KC_TAB,KC_ESC,RCTL_T(KC_QUOT),
+    KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_MINS,KC_EQL, KC_LBRC,RALT_T(KC_RBRC),KC_BSLS,
                     KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS
 ),
 
